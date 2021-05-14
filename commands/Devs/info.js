e1aa2a964c0edd73d5cd79259f721385652ae27a
Joovzx <74085766+Joovzx@users.noreply.github.com>
2021-05-15 06:49:41 +0700

    Simple Command
    Info command in devs category.

--- commands/Devs/info.js
+++ commands/Devs/info.js
@@ -1,1 +1,19 @@
+const { MessageEmbed } = require("discord.js")
 
+module.exports = {
+  name: "info",
+  description: "Show y'all about my devs.",
+  usage: "",
+  aliases: ["inf"],
+  category: "devs",
+  run: async (client, message, args) => {
+    
+    let owner = client.users.cache.get("ID").tag
+    let first = ["#ff6347","#fff578","#3dff31"]; 
+    let kedua = first[Math.floor(Math.random() * first.length)];
+    
+    message.inlineReply(new MessageEmbed()
+    .setTitle("My Devs Is"+owner)
+    .setColor(message.member.displayHexColor))
+
+}} 
